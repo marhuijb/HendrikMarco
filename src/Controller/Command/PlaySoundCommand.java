ecba2057598f15e2acf77181b8aec98202ae45ec
hvanhaele <hvanhaele@MBP2015>
2017-11-13 20:33:10 +0100

    Sound toegevoegd als actie

--- src/Controller/Command/PlaySoundCommand.java
+++ src/Controller/Command/PlaySoundCommand.java
@@ -1,20 +1,41 @@
 package Controller.Command;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+
 /**
  * Command class for playing a sound 
  */
 public class PlaySoundCommand extends CommandDecorator{
-	/*
-	 * Constructor
-	 * @param presentationController 
-	 */
+	private static final String sound = "/sound/yeehaw.wav";
+
 	public PlaySoundCommand() {
 		super(null, null);
 	}
 	
 	public void execute() {
-		java.awt.Toolkit.getDefaultToolkit().beep();
-		
+		play(sound);
 		super.execute();
+	}
+	
+	private void play(String url) {
+		Clip clip = null;
+		try {
+			InputStream in = getClass().getResourceAsStream(url);
+			InputStream buf = new BufferedInputStream(in);
+			AudioInputStream ain = AudioSystem.getAudioInputStream(buf);
+			AudioFormat format = ain.getFormat();
+			DataLine.Info info = new DataLine.Info(Clip.class, format);
+			clip = (Clip) AudioSystem.getLine(info);
+			clip.open(ain);
+			clip.start();
+		} catch (Exception e) {
+		}
 	}
 }
